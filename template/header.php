e190c1fc55e5d7e33372f576cfa3b8b98bb265a5
Siddharth Mahapatra <siddharth.mahapatra.coding@gmail.com>
2018-04-03 15:09:03 +0530

    Add files via upload

---
+++ template/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+
+<html>
+
+	</body>
+	</html>
+	<head>
+
+		<title>CRUD Database App</title>
+
+		<link rel="stylesheet" href="css/style.css">
+	</head>
+
+	<body>
+		<h1>Simple Database App</h1>
